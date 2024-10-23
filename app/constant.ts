dc1796d259f36e090c8d2299ee0a784c73cf4e6a
trondweb <114964140+trondweb@users.noreply.github.com>
2024-10-23 20:50:25 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -501,5 +501,5 @@ export const PLUGINS = [
   { name: "Search Chat", path: Path.SearchChat },
 ];
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
-export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+export const SAAS_CHAT_URL = "https://gpt.cloudaifree.com/chat";
+export const SAAS_CHAT_UTM_URL = "https://gpt.cloudaifree.com/chat?utm=github";
